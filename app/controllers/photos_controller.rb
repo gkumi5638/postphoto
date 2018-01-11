9168e63517cf4d13327fc8a9224f1e78f703d180
masataka <ar.tec.masa@gmail.com>
2018-01-11 23:35:07 +0900

    controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
-	def index
-		@title  = "写真一覧"
-		@photos = Photo.all
-	end
+  def index
+    @title  = "写真一覧"
+    @photos = Photo.all
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,28 @@
 class UsersController < ApplicationController
 
-	def index
-		@title = "ユーザー一覧"
-		@users = User.all
-	end
+  def index
+    @title = "ユーザー一覧"
+    @users = User.all
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-		@user = User.new(user_params)
-	    if @user.save
-	      render :show, status: :created
-	    else
-	      render json: @user.errors, status: :unprocessable_entity
-	    end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-
-
-	private
-		def user_params
-			params.require(:user).permit(:name, :email)
-		end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
